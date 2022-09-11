0587041b473f538f9318c1de8601aa56e838794c
gstfnk <gabrysia.stefaniak@gmail.com>
2022-09-11 18:24:17 +0200

    Adding ingredients to recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,16 +1,39 @@
 import {Recipe} from "./recipe.model";
-import {EventEmitter} from "@angular/core";
+import {EventEmitter, Injectable} from "@angular/core";
+import {Ingredient} from "../shared/ingredient.model";
+import {ShoppingListService} from "../shopping-list/shopping-list.service";
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('Test Recipe', 'Test Desc',
-      'https://cdn-icons-png.flaticon.com/512/4465/4465494.png')
+    new Recipe(
+      'Spaghetti',
+      'Spaghetti Desc',
+      'https://cdn-icons-png.flaticon.com/512/4465/4465494.png',
+      [
+        new Ingredient('Tomatoes', 3),
+        new Ingredient('Meat', 2)
+      ]),
+    new Recipe(
+      'Hamburger',
+      'Hamburger Desc',
+      'https://cdn-icons-png.flaticon.com/512/3075/3075977.png',
+      [
+        new Ingredient('Buns', 1),
+        new Ingredient('Pickles', 2)
+      ]),
   ];
 
+  constructor(private shoppingListService: ShoppingListService) {
+  }
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
   }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,24 @@
-export class ShoppingListService {
+import {EventEmitter} from "@angular/core";
+import {Ingredient} from "../shared/ingredient.model";
 
+export class ShoppingListService {
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  private ingredients: Ingredient[] = [
+    new Ingredient('Tomatoes', 3),
+    new Ingredient('Basil', 1)
+  ];
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
+
+  addIngredients(ingredients: Ingredient[]) {
+    this.ingredients.push(...ingredients);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
 }
